ef5bbd87feb5b03c93b7e214077eebd0d0534257
daniegarcia254 <daniegarcia254@gmail.com>
2015-12-12 21:10:22 +0100

    Change API url base

--- public/js/constants.js
+++ public/js/constants.js
@@ -2,5 +2,5 @@
  * Created by Daniel on 10/10/2015.
  */
 comunioApp.constant('APP_CONSTANTS', {
-        "API_URI" : "http://localhost/comuniazoUK/api"
+        "API_URI" : "http://localhost/comuniazo-uk/api"
     });
