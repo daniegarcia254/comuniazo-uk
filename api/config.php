3fd71d7cc976ad773d809a651853b7d57341cb76
daniegarcia254 <daniegarcia254@gmail.com>
2015-12-29 00:26:42 +0100

    Global constants file

---
+++ api/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('COMMUNITY_NAME','La Premier Toñi');
+define('COMMUNITY_ID','UK782729');
+define('COMMUNITY_TID',179580);
+define('USER_DB','root');
+define('USER_PWD','');
+define('DATABASE','comunio_uk');
+define('HOST', '127.0.0.1');
+define('COMUNIO_COMMUNITY_URL_BASE','http://www.comunio.co.uk/teamInfo.phtml?tid=');
+define('COMUNIO_USER_URL_BASE','http://www.comunio.co.uk/playerInfo.phtml?pid=');
+
+?>
